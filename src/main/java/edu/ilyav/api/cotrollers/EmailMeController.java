602acb86fcc4744d10819b9518768df215fdeb0d
ivald <ivald79@gmail.com>
2018-08-13 23:04:48 -0400

    email me feature fixed

--- src/main/java/edu/ilyav/api/cotrollers/EmailMeController.java
+++ src/main/java/edu/ilyav/api/cotrollers/EmailMeController.java
@@ -2,6 +2,8 @@ package edu.ilyav.api.cotrollers;
 
 import edu.ilyav.api.models.EmailMe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/public")
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties")
 public class EmailMeController {
+
+	@Value("${spring.mail.username}")
+	private String username;
+
+	@Value("${spring.mail.cc}")
+	private String cc;
 
 	@Autowired
 	public JavaMailSender emailSender;
@@ -20,8 +29,9 @@ public class EmailMeController {
 	public EmailMe forgotPassword(@RequestBody EmailMe email) {
 		EmailMe me = new EmailMe();
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(email.getEmail());
-		message.setSubject(email.getName());
+		message.setTo(this.username);
+		message.setCc(this.cc);
+		message.setSubject(email.getName() + " <" + email.getEmail() + '>');
 		message.setText(email.getMessage());
 		try {
 			emailSender.send(message);
@@ -29,7 +39,6 @@ public class EmailMeController {
 		} catch (Exception e) {
 			me.setError("Your message has not been sent.");
 		}
-
 
 		return me;
 	}
